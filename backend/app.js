5f4c560c96a87f305b68bcc8802b713accb79e60
Marina Privalova <privalovama@yandex.ru>
2023-06-14 14:49:15 +0300

    feat: logger

---
+++ backend/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const bodyParser = require('body-parser');
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+const auth = require('./middlewares/auth');
+const { createUser, login } = require('./controllers/users');
+const { signinValidator, signupValidator } = require('./middlewares/validation');
+const NotFoundError = require('./utils/errors/NotFoundError');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+const { PORT = 3000, MONGO_URL = 'mongodb://127.0.0.1/mestodb' } = process.env;
+
+const app = express();
+
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('База данных подключена'))
+  .catch((err) => console.log('Ошибка подключения к БД', err));
+
+mongoose.set({ runValidators: true });
+
+app.use(requestLogger);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/signin', signinValidator, login);
+app.post('/signup', signupValidator, createUser);
+
+app.use(auth);
+app.use('/', userRouter);
+app.use('/', cardRouter);
+
+app.all('/*', (req, res, next) => {
+  next(new NotFoundError('Страница не существует'));
+});
+
+app.use(errorLogger);
+
+app.use(errors());
+app.use((err, req, res, next) => {
+  const {
+    statusCode = 500,
+    message,
+  } = err;
+  res.status(statusCode)
+    .send({
+      message: statusCode === 500 ? 'На сервере произошла ошибка' : message,
+    });
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
---
+++ backend/routes/cards.js
@@ -0,0 +1,21 @@
+const cardRouter = require('express').Router();
+
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
+
+const {
+  cardValidator, cardIdValidator,
+} = require('../middlewares/validation');
+
+cardRouter.get('/cards', getCards);
+cardRouter.post('/cards', cardValidator, createCard);
+cardRouter.delete('/cards/:cardId', cardIdValidator, deleteCard);
+cardRouter.put('/cards/:cardId/likes', cardIdValidator, likeCard);
+cardRouter.delete('/cards/:cardId/likes', cardIdValidator, dislikeCard);
+
+module.exports = cardRouter;
